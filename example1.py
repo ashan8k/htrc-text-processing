f779f8e1380edc3285ba3957aeb260cb47ad1dce
ashan8k <ashan8k@gmail.com>
2020-06-29 20:22:02 +0200

    get_zip_only and get_zips_extract

--- example1.py
+++ example1.py
@@ -1,4 +1,7 @@
 import htrc_text_processing as htrc_tp
 
+# If you only need to get ZIP files into a directory
+htrc_tp.get_zips_only('sample-pairtree-data', 'output_only_zip_files')
 
-htrc_tp.get_zips('sample-pairtree-data', 'zip_files')
+# If you need to get extracted zip folders into a directory
+htrc_tp.get_zips_extract('sample-pairtree-data', 'output_unziped_files')
--- htrc_text_processing/__init__.py
+++ htrc_text_processing/__init__.py
@@ -1,9 +1,10 @@
 import glob
 import os
 import shutil
+from zipfile import ZipFile
 
 
-def get_zips(data_dir, output_dir):
+def get_zips_only(data_dir, output_dir):
     if os.path.isdir(data_dir):
         if not os.path.isdir(output_dir):
             try:
@@ -14,7 +15,30 @@ def get_zips(data_dir, output_dir):
                 print("Successfully created the directory %s " % output_dir)
 
         for x in glob.glob(data_dir + '/**/*.zip', recursive=True):
-            print(x)
+            # print(x)
             shutil.copy(x, output_dir)
     else:
         raise Exception(data_dir + ' path does not exists!')
+
+
+def get_zips_extract(data_dir, output_dir):
+    if os.path.isdir(data_dir):
+        if not os.path.isdir(output_dir):
+            try:
+                os.mkdir(output_dir)
+            except OSError:
+                print("Creation of the directory %s failed" % output_dir)
+            else:
+                print("Successfully created the directory %s " % output_dir)
+
+        for x in glob.glob(data_dir + '/**/*.zip', recursive=True):
+            # print(x)
+            with ZipFile(x, 'r') as zipObj:
+                zipObj.extractall(output_dir)
+    else:
+        raise Exception(data_dir + ' path does not exists!')
+
+
+def unzip_file(file_name):
+    with ZipFile(file_name, 'r') as zipObj:
+        zipObj.extractall()
