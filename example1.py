8be87c17f4ed23d9d15e165ea98523613f6992bc
ashan8k <ashan8k@gmail.com>
2020-07-08 18:46:27 +0200

    added clean_path, load_vol, clean_vol

--- example1.py
+++ example1.py
@@ -1,7 +1,20 @@
-import htrc_text_processing as htrc_tp
+import tqdm
 
+import htrc_text_processing as htrc_tp
+from pathlib import Path
 # If you only need to get ZIP files into a directory
-htrc_tp.get_zips_only('sample-pairtree-data', 'output_only_zip_files')
+# htrc_tp.get_zips_only('sample-pairtree-data', 'output_only_zip_files')
 
 # If you need to get extracted zip folders into a directory
-htrc_tp.get_zips_extract('sample-pairtree-data', 'output_unziped_files')
+# htrc_tp.get_zips_extract('sample-pairtree-data', 'output_unziped_files')
+
+# clean file names if Path Exists
+# giving Warning if file not exists
+# htrc_tp.clean_file_name('output_unziped_files/ark+=13960=t3mw3px6k/something_00000001.txt')
+
+# load text files
+txt_files = htrc_tp.load_vol('output_unziped_files/ark+=13960=t3mw3px6k',2)
+
+x = htrc_tp.swinburne_clean_vol(['output_unziped_files/ark+=13960=t3mw3px6k/00000001.txt'], 'temp')
+
+
--- htrc_text_processing/__init__.py
+++ htrc_text_processing/__init__.py
@@ -2,7 +2,15 @@ import glob
 import os
 import shutil
 from zipfile import ZipFile
+from pathlib import Path
+import warnings
 
+from typing import List
+
+from tqdm import tqdm
+
+from htrc.models import HtrcPage
+from htrc.runningheaders import parse_page_structure
 
 def get_zips_only(data_dir, output_dir):
     if os.path.isdir(data_dir):
@@ -42,3 +50,56 @@ def get_zips_extract(data_dir, output_dir):
 def unzip_file(file_name):
     with ZipFile(file_name, 'r') as zipObj:
         zipObj.extractall()
+
+
+def clean_file_name(file):
+    file_path = Path(file)
+    if file_path.exists():
+        print(file_path.name)
+        os.rename(file_path, file_path.parent / file_path.name.split("_")[-1])
+    else:
+        warnings.warn("Path/File not exists. Path = " + file)
+
+
+def load_vol(path: str, num_pages: int) -> List[HtrcPage]:
+    pages = []
+    py_num_pages = num_pages - 1
+    for n in range(py_num_pages):
+        if n == 0:
+            n = 1
+            page_num = str(n).zfill(8)
+            with open('{}/{}.txt'.format(path, page_num), encoding='utf-8') as f:
+                lines = [line.rstrip() for line in f.readlines()]
+                pages.append(HtrcPage(lines))
+        else:
+            page_num = str(n).zfill(8)
+            with open('{}/{}.txt'.format(path, page_num), encoding='utf-8') as f:
+                lines = [line.rstrip() for line in f.readlines()]
+                pages.append(HtrcPage(lines))
+
+    return pages
+
+
+def swinburne_clean_vol(vol_dir_path_list: list, out_dir: str):
+    vol_num = 0
+    for vol_dir_path in tqdm(vol_dir_path_list):
+        print(f"this is vol_dir_path: {vol_dir_path}")
+        filename = vol_dir_path.split("/", -1)[-2]
+        # print(f"this is filename: {filename}")
+        page_paths = sorted(glob.glob(vol_dir_path + '/*.txt'))
+        # print(page_paths)
+        file_count = len(page_paths)
+        loaded_vol = load_vol(vol_dir_path, file_count)
+        pages = parse_page_structure(loaded_vol)
+        outfile = filename + '.txt'
+        # print(outfile)
+        vol_num += 1
+
+        with open(outfile, 'w') as f:
+            clean_file_path = os.getcwd() + '/' + outfile
+            for n, page in enumerate(pages):
+                # print('.')
+                f.write(page.body + '\n')
+            shutil.move(clean_file_path, out_dir)
+
+    return print(f"Cleaned {vol_num} volume(s)")
