1819107a34e128762413a6f07b9d2513620b9b3e
ashan8k <ashan8k@gmail.com>
2020-08-02 23:51:54 +0200

    bug fixes function renames

--- example.py
+++ example.py
@@ -1,12 +1,32 @@
+import glob
+import time
+
 import htrc_text_processing as htrc_tp
 from pathlib import Path
 
 # to get extracted ZIP files to a folder
 # htrc_text_processing.get_zips('<path to pairtree parent/s>', 'path to output directory')
-htrc_tp.get_zips('data_sets/sample-pairtree-data-set-2', 'outputs/unzipped')
+# htrc_tp.get_zips('data_sets/sample-pairtree-data-set-2', 'outputs/unzipped2')
 
 # clean file names if Path Exists
 # giving Warning if file not exists
 # htrc_tp.clean_file_name('outputs/zips/35556044272359/something_00000001.txt')  # file will renamed to 00000001.txt
 
+# htrc_tp.clean_txt_file_names('outputs/unzips2/39002002224252/')
+# htrc_tp.normalize_txt_file_names('outputs/unzips2/39015059701311/')
+
+page_directory_list = glob.glob('outputs/unzipped2/*/')
+print(len(page_directory_list))
+for f in page_directory_list:
+   # htrc_tp.normalize_txt_file_names(f)
+   print(f)
+    #time.sleep(1)
+
+htrc_tp.normalize_txt_file_names('outputs/unzipped2/39002018496662/')
 # htrc_tp.clean_vol(['outputs/unziped/39002002224252'], 'temp')
+clean_vol_out_dir = 'outputs/cleaned_vols'
+page_directory_list = glob.glob('outputs/unzipped2/*/')
+print(len(page_directory_list))
+htrc_tp.clean_vol(page_directory_list, clean_vol_out_dir)
+
+#htrc_tp.clean_vol(page_directory_list,1)
--- htrc_text_processing/__init__.py
+++ htrc_text_processing/__init__.py
@@ -4,13 +4,30 @@
 import glob
 import os
 import shutil
+import sys
 from zipfile import ZipFile
 from pathlib import Path
-import warnings
+# import warnings
 from typing import List
 from tqdm import tqdm
 from htrc_text_processing.htrc.models import HtrcPage
 from htrc_text_processing.htrc.runningheaders import parse_page_structure
+
+CRED = '\033[91m'
+CEND = '\033[0m'
+CURL = '\33[4m'
+CGREEN = '\33[32m'
+CGREY = '\33[100m'
+CBLINK = '\33[5m'
+
+
+def unzip_file(file_name):
+    with ZipFile(file_name, 'r') as zipObj:
+        zipObj.extractall()
+
+
+def error_message(msg):
+    sys.exit(CRED + msg + CEND)
 
 
 def get_zips(data_dir, output_dir, cmd='x'):
@@ -50,18 +67,94 @@ def get_zips(data_dir, output_dir, cmd='x'):
         raise Exception(data_dir + ' path does not exists!')
 
 
-def unzip_file(file_name):
-    with ZipFile(file_name, 'r') as zipObj:
-        zipObj.extractall()
-
-
-def clean_file_name(file):
+def clean_txt_file_names(file):
     file_path = Path(file)
-    if file_path.exists():
-        print(file_path.name)
-        os.rename(file_path, file_path.parent / file_path.name.split("_")[-1])
+    if file_path.exists() and file_path.is_file():
+        if '.txt' in str(file_path):
+            print(file_path.name)
+            os.rename(file_path, file_path.parent / file_path.name.split("_")[-1])
+        else:
+            error_message("Not a txt file\nInvalid txt file: " + str(file_path))
+
+    elif file_path.exists() and file_path.is_dir():
+        # print('dir')
+        for x in tqdm(glob.glob(str(file_path) + '/*.txt', recursive=True)):
+            # print(x)
+            x_path = Path(x)
+            os.rename(x_path, x_path.parent / x_path.name.split("_")[-1])
     else:
-        warnings.warn("Path/File not exists. Path = " + file)
+        error_message("Path/File not exists. Path = " + str(file_path))
+
+
+def is_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def normalize_txt_file_names(dir_or_file):
+    dir_or_file_path = Path(dir_or_file)
+    if dir_or_file_path.exists() and dir_or_file_path.is_dir():
+        print('txt file name cleaning started!')
+        clean_txt_file_names(dir_or_file_path)
+        print('txt file name cleaning done!')
+        txt_files = []
+        for x in (glob.glob(str(dir_or_file_path) + '/*.txt', recursive=True)):
+            x_path = Path(x)
+            txt_file_name = x_path.name.split(".")[0]
+            if is_integer(txt_file_name):
+                # print(txt_file_name)
+                txt_files.append(txt_file_name)
+            else:
+                error_message("Invalid txt file format \nInvalid txt file: " + str(x_path))
+
+        if not txt_files:
+            error_message("No txt filed found in " + str(dir_or_file_path) +
+                          "\nPlease give a directory which have txt files")
+
+        ll = sorted(txt_files)
+        count = int(min(ll))
+        renamed_list = []
+        for i in tqdm(ll):
+            if count != int(i) or len(i) != 8:
+                file_path = dir_or_file_path / (i + ".txt")
+                file_path_replace = dir_or_file_path / (
+                        ''.join([char * (8 - len(str(count))) for char in '0']) + str(count) + ".txt")
+                os.rename(file_path, file_path_replace)
+                renamed_list.append(
+                    str(file_path) + CBLINK + " -> " + CEND + CGREEN + str(file_path_replace) + CEND)
+                # print(file_path, file_path_replace)
+                # print(str(dir_path) + "/" + i + "," + str(dir_path) + "/" + ''.join(
+                #    [char * (len(ll[0]) - len(str(count))) for char in '0']) + str(count))
+            count += 1
+
+        if not renamed_list:
+            print("No normalization is needed!")
+        else:
+            print("Normalized files")
+            print("\n".join(renamed_list))
+
+    elif dir_or_file_path.exists() and dir_or_file_path.is_file():
+        print('txt file name cleaning started!')
+        clean_txt_file_names(dir_or_file)
+        print('txt file name cleaning done!')
+        txt_file_name = (dir_or_file_path.name.split(".")[0]).split("_")[-1]
+        # print(txt_file_name)
+        file_path = dir_or_file_path.parent / (txt_file_name + ".txt")
+        if is_integer(txt_file_name):
+            value = int(txt_file_name)
+            file_path_replace = dir_or_file_path.parent / (
+                    ''.join([char * (8 - len(str(value))) for char in '0']) + str(value) + ".txt")
+            # print(file_path)
+            # print(file_path_replace)
+            os.rename(file_path, file_path_replace)
+        else:
+            error_message("Invalid txt file format \nInvalid txt file: " + str(file_path))
+
+    else:
+        error_message("Directory/File not exists !!!. \nDirectory/File Path = " + str(dir_or_file_path))
 
 
 def load_vol(path: str, num_pages: int) -> List[HtrcPage]:
@@ -85,6 +178,10 @@ def load_vol(path: str, num_pages: int) -> List[HtrcPage]:
 
 def clean_vol(vol_dir_path_list: list, out_dir: str):
     vol_num = 0
+
+    assert isinstance(vol_dir_path_list, list), 'clean_vol() 1st parameter vol_dir_path_list="{}" not of <class "list">'.format(vol_dir_path_list)
+    assert isinstance(out_dir, str), 'clean_vol() 2nd parameter out_dir="{}" not of <class "str">'.format(
+        out_dir)
 
     for vol_dir_path in tqdm(vol_dir_path_list):
         print(f"this is vol_dir_path: {vol_dir_path}")
